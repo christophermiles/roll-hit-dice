e15c1ea8ae7816b477a04b8a0779fb58261ad0b1
Christopher Miles <cmiles@unimelb.edu.au>
2024-10-09 11:13:18 +1100

    Added TypeScript

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type DiceCount = number
+export type DieType = number
+export type Modifier = number
+
+export interface HitPointResults {
+    minimum: number
+    weak: number
+    average: number
+    strong: number
+    maximum: number
+}
