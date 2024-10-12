44fa1c2c69ce1f5dcde7a07363c8e46fd52798c2
Christoper Miles <mail@christophermiles.com.au>
2024-10-12 21:11:08 +1100

    Added option to strictly check for d4, d6, d8, d10, d12 or d20

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const HIT_DICE_REGEX = /^(\d+)d(\d+)([+-]\d+)?$/
+
+export const HIT_DICE_REGEX_STRICT = /^(\d+)d(4|6|8|10|12|20)([+-]\d+)?$/
--- src/roll-hit-dice.ts
+++ src/roll-hit-dice.ts
@@ -1,13 +1,13 @@
 import type { DiceCount, DieType, HitPointResults, Modifier } from './types'
-import { HIT_DICE_REGEX } from './constants'
+import {HIT_DICE_REGEX, HIT_DICE_REGEX_STRICT} from './constants'
 import { calculateMiddle, calculateMinimum, getDieAverage } from './utils'
 
-export function parseHitDice(expression: string): {
+export function parseHitDice(expression: string, strict = false): {
     diceCount: DiceCount
     dieType: DieType
     modifier: Modifier
 } {
-    const match = expression.match(HIT_DICE_REGEX)
+    const match = expression.match(strict ? HIT_DICE_REGEX_STRICT : HIT_DICE_REGEX)
 
     if (match) {
         const diceCount: DiceCount = Number.parseInt(match[1], 10)
@@ -25,8 +25,8 @@ export function parseHitDice(expression: string): {
     }
 }
 
-export default function rollHitDice(expression: string): HitPointResults {
-    const { diceCount, dieType, modifier } = parseHitDice(expression)
+export default function rollHitDice(expression: string, strict = false): HitPointResults {
+    const { diceCount, dieType, modifier } = parseHitDice(expression, strict)
 
     if (Number.isNaN(diceCount) || Number.isNaN(dieType) || diceCount <= 0 || dieType <= 0) {
         throw new Error('🎲 Die type and number of dice must be positive integers.')
